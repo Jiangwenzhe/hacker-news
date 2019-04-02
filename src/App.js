e94e6865bd987569d9d4a982e14bc37ac9c5809d
Erzhuyijian <1821344102@qq.com>
2019-04-02 23:07:36 +0800

    modify index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+    };
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      xsa
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -10,3 +10,8 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+//应用热调试
+if (module.hot) {
+  module.hot.accept();
+}
