2b7abd9139f959b0772bf141773b6558a40b6463
Erzhuyijian <1821344102@qq.com>
2019-04-03 13:16:17 +0800

    modify index.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 
+const largeColumn = { width: '40%',};
+const midColumn = { width: '30%',};
+const smallColumn = { width: '10%',};
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,19 +17,122 @@ class App extends Component {
       result: null,
       searchTerm: DEFAULT_QUERY,
     };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+
   }
 
   setSearchTopStories(result) {
     this.setState({ result });
   }
 
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`,{
+      method: 'GET',
+      mode: 'cors',
+      cache: 'default'
+    })
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .then(e => e);
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  onDismiss(id) {
+   const isNotId = item => item.objectID !== id;
+   const updatedList = this.state.result.hits.filter(isNotId);
+   this.setState({
+     //result: Object.assign({}, this.state.result, { hits: updatedList })
+     result: { ...this.state.result, hits: updatedList }
+    });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+
   render() {
+    console.log(this.state.result);
+    //解构赋值
+    const { result, searchTerm } = this.state;
     return (
-      <div className="App">
-      xsa
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange = {this.onSearchChange}
+            onSubmit = {this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        {/* 条件渲染，如果result是空的话，就直接跳过，如果不是，就接着渲染table组件 */}
+        { result &&
+          <Table
+            list={result.hits}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, onSubmit ,children }) =>
+  <form onSubmit={onSubmit}>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+    <button type="submit">
+      {children}
+    </button>
+  </form>
+
+const Table = ({ list, onDismiss }) =>
+  <div className="table">
+    {list.map(item =>
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({ onClick, className='', children }) =>
+  <button
+    onClick={onClick}
+    className={className}
+    children={children}
+  >
+    {children}
+  </button>
 
 export default App;
